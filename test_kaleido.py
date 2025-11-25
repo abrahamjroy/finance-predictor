68c20c2185a48b2f31c85fac0e1815f97492caec
Abraham Jeevan Roy <abrahamjroy@gmail.com>
2025-11-25 18:31:41 +0530

    Remove unused legacy files

--- test_kaleido.py
+++
@@ -1,7 +0,0 @@
-import plotly.graph_objects as go
-try:
-    fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
-    img_bytes = fig.to_image(format="png")
-    print("Kaleido export successful")
-except Exception as e:
-    print(f"Kaleido export failed: {e}")
